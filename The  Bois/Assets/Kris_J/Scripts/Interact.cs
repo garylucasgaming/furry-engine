78b7287c2f90c9c3719dcbe5512d02cfb84bdaa6
Kris J <KrisJohnson458@gmail.com>
2020-05-24 14:13:47 -0500

    Pushing Scripts
    Pushing to combine master with my branch

---
+++ The  Bois/Assets/Kris_J/Scripts/Interact.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Script to handle interactable objects
+// Kris J
+
+public class Interact : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas interactUI;
+    private Player UserPlayer;
+
+    private void Start() { 
+    
+        interactUI = GetComponentInChildren<Canvas>(true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.tag == "Player")
+        {
+            UserPlayer = player.GetComponent<Player>();
+            UserPlayer.canInteract = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D player)
+    {
+        if(player.tag == "Player")
+        {
+            UserPlayer = player.GetComponent<Player>();
+            UserPlayer.canInteract = false;
+        }
+    }
+}
