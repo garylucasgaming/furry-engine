b9fb01491a893e8b71b0cbc15e401ff3fc52dec6
Vincent Manno <vincent.s.manno@gmail.com>
2020-05-26 15:06:42 -0400

    Basically Finished Game
    yay

---
+++ The  Bois/Assets/Scripts/Trader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trader : MonoBehaviour
+{
+    [SerializeField] InventoryManager IM;
+    [SerializeField] Player player;
+
+    public void SellPotions()
+    {
+        Debug.Log("Selling potions");
+
+        if (IM.inventory.GetItemList().Count == 0)
+        {
+            //Tell player they don't have items/potions
+            return;
+        }
+        else
+        {
+            for (int i = IM.inventory.GetItemList().Count - 1; i >= 0; i--)
+            {
+                Item item = IM.inventory.GetItemList()[i];
+                if (item.itemType == Item.ItemType.HealingPotion ||
+                    item.itemType == Item.ItemType.StrengthPotion)
+                {
+                    IM.inventory.RemoveItem(item);
+                    player.money++;
+                }
+            }
+        }
+    }
+}
---
+++ The  Bois/Assets/Scripts/UI_Money.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Money : MonoBehaviour
+{
+    [SerializeField] Player player;
+    private void Update()
+    {
+        GetComponent<TMPro.TMP_Text>().text = $"Money: {player.money}";
+    }
+}
